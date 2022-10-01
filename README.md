0c2d228b910a0e1bb7767f0198c9cc39f8d60374
johnlombardi389 <lombardij38@gmail.com>
2022-10-01 16:06:29 -0400

    Fetched openweather API

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Tools:
 - HTML
 - CSS
 - JavaScript
+- API
--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+const weather = document.getElementsByClassName("weather");
 const city = document.getElementsByClassName("city");
 const temp = document.getElementsByName("temp");
 const description = document.getElementsByClassName("description");
+const feels_like = document.getElementsByClassName("feels-like");
+const pressure = document.getElementsByClassName("pressure");
 const humidity = document.getElementsByClassName("humidity");
 const wind = document.getElementsByClassName("wind");
 
@@ -13,6 +16,24 @@ function getWeather() {
 
     fetchWeather(
       `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
-    );
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        showWeather(data);
+      });
   });
+}
+getWeather();
+
+function showWeather(data) {
+  let { temp, description, feels_like, pressure, humidity, wind_speed } = data.current;
+
+  weather.innerHTML = `<h2 class="city">Weather in City</h2>
+  <h1 class="temp">${temp}</h1>
+  <div class="description">${description}</div>
+  <div class="feels-like">Feels like: ${feels_like}</div>
+  <div class="pressure">Pressure: ${pressure}</div>
+  <div class="humidity">Humidity: ${humidity}</div>
+  <div class="wind">Wind Speed: ${wind_speed}</div>;
 }
